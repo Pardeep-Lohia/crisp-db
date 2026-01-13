cc20f31c29d47ad3749754107cd56a938deed4f1
kunal <kunalcode23106052@gmail.com>
2026-01-13 16:08:52 +0530

    Add database connection function for ChatBot

---
+++ Database_function/connect_db.py
@@ -0,0 +1,27 @@
+import psycopg
+import os
+from dotenv import load_dotenv
+
+# make the connection to .env
+load_dotenv()
+
+# Connect to ChatBotdb
+def get_conn():
+    """
+    Return a PostgreSQL connection to ChatBotdb
+    """
+    conn = psycopg.connect(
+        host=os.getenv("POSTSQL_HOST"),        
+        dbname="chatbot_saas",
+        user=os.getenv("POSTSQL_USER"),        
+        password=os.getenv("POSTSQL_PASSWORD"),
+        port=os.getenv("POSTSQL_PORT") 
+    )
+    return conn
+
+
+# Test query
+if __name__=="__main__":
+  cur = get_conn().cursor()
+  cur.execute("SELECT 1;")
+  print(cur.fetchone())  # Output: (1,)
